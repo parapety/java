9288e02431816549177024a24f9b5e45c94c85fe
Marcin Wrobel <marcin.wrobel@schibsted.pl>
2017-03-01 13:12:20 +0100

    refactor test therories instead hashmap

--- tests/org/wr/PeselProcessorTest.java
+++ tests/org/wr/PeselProcessorTest.java
@@ -3,34 +3,58 @@ package org.wr;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class PeselProcessorTest {
 
-	@Test
-	public void shouldReturnExpectedDate() {
-		// given
-		HashMap<String, String> dataProvider = new HashMap<String, String>();
+	@DataPoints("AAA")
+	public static Set<Map.Entry<String, String>> dataProviderSet;
+	static{
+		
+		Map<String,String> dataProvider=new HashMap<>();
 		dataProvider.put("44051401458", "1944-05-14");
 		dataProvider.put("85832212346", "1885-03-22");
-		dataProvider.put("85632212340", "2285-03-22");
+		
 		dataProvider.put("85432212344", "2185-03-22");
-		dataProvider.put("85232212348", "2085-03-22");
-		PeselProcessor processor = new PeselProcessor();
+		dataProvider.put("85232212348", "2085-03-22");		
+		dataProviderSet=dataProvider.entrySet();
+	}
 
-		Set set = dataProvider.entrySet();
-		Iterator iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Map.Entry mentry = (Map.Entry) iterator.next();
+	@DataPoints("BBB")
+	public static Set<Map.Entry<String, String>> dataProviderSetb;
+	static{
+		
+		Map<String,String> dataProvider=new HashMap<>();
+		dataProvider.put("44051401458", "1944-05-14");
+		dataProvider.put("85832212346", "1885-03-22");
+		dataProvider.put("85632212340", "6666-03-22");
+		dataProvider.put("85432212344", "2185-03-22");
+		dataProvider.put("85232212348", "2085-03-22");		
+		dataProviderSetb=dataProvider.entrySet();
+	}
+	
+	
+	private PeselProcessor processor = new PeselProcessor();
+	
+	@Theory
+	public void shouldReturnExpectedDate(@FromDataPoints("AAA")Map.Entry<String, String> entry) {
+		
 			// when
-			String result = processor.readBirthDate((String) mentry.getKey());
+			String result = processor.readBirthDate((String) entry.getKey());
 			// then
-			assertEquals(result, mentry.getValue());
-		}
+			assertEquals(result, entry.getValue());
+	
 	}
 
 	@Test
